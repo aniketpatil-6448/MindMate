ff1e89a18769aaa62bed03e75104a9603f15f599
Aniket Dattusing Patil <116289643+aniketpatil-6448@users.noreply.github.com>
2025-11-05 00:24:14 +0530

    Change server URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ import ViewLecture from './pages/ViewLecture'
 import SearchWithAi from './pages/SearchWithAi'
 import getAllReviews from './customHooks/getAllReviews'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://mindmate-1-scqp.onrender.com"
 
 function App() {
   
